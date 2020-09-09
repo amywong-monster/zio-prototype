375d1370ecc5bbf6f744ff30d5916f89c31f2a3c
Amy Wong <amy.wong@monster.com>
2020-09-10 02:01:39 +1000

    Move `sql` inside the function of `DbUtils`

--- src/main/scala/org/jinilover/microservice/persistence/DBUtils.scala
+++ src/main/scala/org/jinilover/microservice/persistence/DBUtils.scala
@@ -8,18 +8,19 @@ import zio.{ Has, Task, ZIO }
 import zio.interop.catz._
 
 object DBUtils {
-  val sql =
-    s"""
+  def createSchema: ZIO[Has[Migrations.Service] with Has[Transactor[Task]], Throwable, Unit] = {
+    val sql =
+      s"""
       DROP SCHEMA public CASCADE;
       CREATE SCHEMA public;
       GRANT ALL ON SCHEMA public TO postgres;
       GRANT ALL ON SCHEMA public TO public;
     """
 
-  def createSchema: ZIO[Has[Migrations.Service] with Has[Transactor[Task]], Throwable, Unit] =
     for {
       xa <- ZIO.access[Has[Transactor[Task]]](_.get)
       _  <- Update0(sql, None).run.transact(xa)
       _  <- ZIO.accessM[Has[Migrations.Service]](_.get.migrate)
     } yield ()
+  }
 }
